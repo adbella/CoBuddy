2e4744e92d7bd2b647c68fe259f7079e4c13ce23
adbella <ajh86463447@gmail.com>
2026-01-31 22:24:22 +0900

    streaming output

--- ai_utils.py
+++ ai_utils.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 import concurrent.futures
+import ai_utils as ai
 from google.genai import Client
 from google.genai.errors import APIError
 from google import genai
